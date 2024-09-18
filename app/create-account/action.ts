c0c034eb3abbd2a891548f748e31a35cce6cf861
moomooj <wndud36395@gmail.com>
2024-09-18 10:43:43 -0700

    superRefine

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -7,6 +7,9 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/session";
+import { error } from "console";
 
 const checkUsername = (username: string) => !username.includes("admin");
 
@@ -18,19 +21,6 @@ const checkPassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
-const checkUniqueEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      id: true,
-    },
-  });
-
-  return !Boolean(user);
-};
-
 const formSchema = z
   .object({
     username: z
@@ -41,14 +31,7 @@ const formSchema = z
       .min(3, "Way too short")
       .max(10, "That is too long")
       .refine(checkUsername, "admin is not allowed"),
-    email: z
-      .string()
-      .email()
-      .toLowerCase()
-      .refine(
-        checkUniqueEmail,
-        "There is an account already registered with that email."
-      ),
+    email: z.string().email().toLowerCase(),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
@@ -58,6 +41,25 @@ const formSchema = z
   .refine(checkPassword, {
     message: "Both passwords should be same",
     path: ["confirm_password"],
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This email is already taken",
+        path: ["email"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
@@ -83,5 +85,9 @@ export async function createAccount(prevState: any, formData: FormData) {
         id: true,
       },
     });
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    redirect("/profile");
   }
 }
