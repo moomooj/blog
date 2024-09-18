3ecee23666241a7873050f41f450455ed15caa77
moomooj <wndud36395@gmail.com>
2024-09-18 08:25:33 -0700

    hashed password and create user

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,4 +1,5 @@
 "use server";
+import bcrypt from "bcrypt";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
@@ -70,6 +71,17 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log("good");
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+    const user = await db.user.create({
+      data: {
+        name: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
   }
 }
