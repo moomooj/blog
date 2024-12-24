eb4ef0e9f198a5c1045918f6ede82fca3d082465
moomooj <wndud36395@gmail.com>
2024-12-23 20:19:53 -0800

    vaildate set up

--- app/(auth)/create-account/action.ts
+++ app/(auth)/create-account/action.ts
@@ -8,7 +8,7 @@ import {
 import db from "@/lib/db";
 import { z } from "zod";
 
-/* const checkUsername = (username: string) => !username.includes("admin");
+const checkUsername = (username: string) => !username.includes("admin");
 
 const checkPassword = ({
   password,
@@ -18,88 +18,43 @@ const checkPassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
-
 const formSchema = z
   .object({
     username: z
       .string({
         invalid_type_error: "Username must be a string",
-        required_error: "where is my username?",
+        required_error: "This field is required.",
       })
-      .min(3, "Way too short")
-      .max(10, "That is too long")
-      .refine(checkUsername, "admin is not allowed"),
-    email: z.string().email().toLowerCase(),
+      .min(3, "Create an Username at least 3 characters long.")
+      .max(10, "Enter an Username under 10 characters.")
+      .trim()
+      .toLowerCase()
+      .refine(checkUsername, "this Username is not allowed"),
+    email: z
+      .string({
+        invalid_type_error: "Email must be a string",
+        required_error: "This field is required.",
+      })
+      .email()
+      .toLowerCase(),
     password: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
+      .min(
+        PASSWORD_MIN_LENGTH,
+        "Create an Password at least 4 characters long."
+      )
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+    confirm_password: z
+      .string()
+      .min(
+        PASSWORD_MIN_LENGTH,
+        "Create an Password at least 4 characters long."
+      ),
   })
   .refine(checkPassword, {
     message: "Both passwords should be same",
     path: ["confirm_password"],
-  })
-  .superRefine(async ({ email }, ctx) => {
-    const user = await db.user.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        id: true,
-      },
-    });
-    if (user) {
-      ctx.addIssue({
-        code: "custom",
-        message: "This email is already taken",
-        path: ["email"],
-        fatal: true,
-      });
-      return z.NEVER;
-    }
   });
-
-export async function createAccount(prevState: any, formData: FormData) {
-  const data = {
-    username: formData.get("username"),
-    email: formData.get("email"),
-    password: formData.get("password"),
-    confirm_password: formData.get("confirm_password"),
-  };
-
-  const result = await formSchema.safeParseAsync(data);
-  
-  if (!result.success) {
-    return result.error.flatten();
-  } else {
-    const hashedPassword = await bcrypt.hash(result.data.password, 12);
-
-    const user = await db.user.create({
-      data: {
-        name: result.data.username,
-        email: result.data.email,
-        password: hashedPassword,
-      },
-      select: {
-        id: true,
-      },
-    });
-    const session = await getSession();
-    session.id = user.id;
-    await session.save();
-    redirect("/profile");
-  }
-}
-*/
-
-const formSchema = z.object({
-  username: z.string().min(3).max(8),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
-
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get("username"),
@@ -111,5 +66,7 @@ export async function createAccount(prevState: any, formData: FormData) {
 
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
