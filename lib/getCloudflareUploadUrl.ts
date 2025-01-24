a8e3b3410c08cd9239352b5e24dd06ca22005282
moomooj <wndud36395@gmail.com>
2025-01-23 21:56:41 -0800

    0.0.7 update

--- lib/getCloudflareUploadUrl.ts
+++ lib/getCloudflareUploadUrl.ts
@@ -10,7 +10,6 @@ export async function getCloudflareUploadUrl() {
       },
     }
   );
-  console.log(response);
   const data = await response.json();
   const { success, result } = data;
   if (success) {
